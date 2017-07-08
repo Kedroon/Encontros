2aa2ee556c6d8cfef9b96523131b29cc47702404
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2017-07-08 13:35:58 -0400

    Update de referencias

--- Encontros/Models/FotoEncontro.cs
+++ Encontros/Models/FotoEncontro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,17 @@ namespace Encontros.Models
         public string Name { get; set; }
         public int Length { get; set; }
         public string Type { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        private DateTime? createdDate;
+
+        [Required]
+        public DateTime CreatedDate
+        {
+            get { return createdDate ?? DateTime.UtcNow; }
+            set { createdDate = value; }
+        }
     }
 }
