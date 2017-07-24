6ad132a13d2d4adfa09b1047dc53fc0426561dba
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2017-07-23 20:31:31 -0400

    Adicionar o nome

--- Encontros/Models/AccountViewModels.cs
+++ Encontros/Models/AccountViewModels.cs
@@ -65,19 +65,23 @@ namespace Encontros.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Senha")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Senha")]
+        [Compare("Password", ErrorMessage = "A senha e a senha de confirmação não são iguais.")]
         public string ConfirmPassword { get; set; }
     }
 
